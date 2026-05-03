223b34d764ee9cdaaed746f362768ac754490a47
Kyle Meyer <kaiuhl@gmail.com>
2026-05-03 11:49:46 -0700

    Fix Que worker startup

--- jobs/worker.rb
+++ jobs/worker.rb
@@ -1,5 +1,13 @@
-require_relative "../config/boot"
-require_relative "fire_restriction_jobs"
+worker_count = ENV.fetch("QUE_WORKER_COUNT", "2")
+poll_interval = ENV.fetch("QUE_POLL_INTERVAL_SECONDS", "5")
 
-BFP.db
-Que.run!
+exec(
+  "bundle",
+  "exec",
+  "que",
+  "--worker-count",
+  worker_count,
+  "--poll-interval",
+  poll_interval,
+  "./jobs/fire_restriction_jobs"
+)
